0896855eeb75766e4327728dd166397d8ddec385
Anup Gupta <anuup22.cs@gmail.com>
2025-02-10 22:21:47 +0530

    enable asynchronous processing in NotificationServerApplication

--- microservices/notification-server/src/main/java/com/anup/ecommerce/NotificationServerApplication.java
+++ microservices/notification-server/src/main/java/com/anup/ecommerce/NotificationServerApplication.java
@@ -2,8 +2,10 @@ package com.anup.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NotificationServerApplication {
 
 	public static void main(String[] args) {
