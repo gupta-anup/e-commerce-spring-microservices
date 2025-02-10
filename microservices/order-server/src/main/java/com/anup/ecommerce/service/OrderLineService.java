7d8219ce22e66e13ab2aea6f0ed19c758f2f2600
Anup Gupta <anuup22.cs@gmail.com>
2025-02-10 15:47:30 +0530

    todo: integrate payment service

--- microservices/order-server/src/main/java/com/anup/ecommerce/service/OrderLineService.java
+++ microservices/order-server/src/main/java/com/anup/ecommerce/service/OrderLineService.java
@@ -1,11 +1,14 @@
 package com.anup.ecommerce.service;
 
 import com.anup.ecommerce.dto.request.OrderLineCreateRequest;
+import com.anup.ecommerce.dto.response.OrderLineResponse;
 import com.anup.ecommerce.entity.OrderLine;
 import com.anup.ecommerce.mapper.OrderLineMapper;
 import com.anup.ecommerce.repository.OrderLineRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +22,11 @@ public class OrderLineService {
         OrderLine savedOrderLine = orderLineRepository.save(orderLine);
 
         return savedOrderLine.getId();
+    }
+
+    public List<OrderLineResponse> getOrderLinesByOrderId(Long orderId) {
+        List<OrderLine> orderLines = orderLineRepository.findAllByOrderId(orderId);
+
+        return OrderLineMapper.toResponse(orderLines);
     }
 }
