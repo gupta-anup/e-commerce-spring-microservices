6a32e7c44b4efa6d7229b42ffe805086754f2194
-g <anuup22.cs@gmail.com>
2025-02-17 17:08:27 +0530

    feat(response): update ProductResponse to use CategoryResponse object

--- microservices/product-server/src/main/java/com/anup/ecommerce/dto/response/ProductResponse.java
+++ microservices/product-server/src/main/java/com/anup/ecommerce/dto/response/ProductResponse.java
@@ -1,17 +1,17 @@
 package com.anup.ecommerce.dto.response;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Builder
+@Data
 public class ProductResponse {
     private Integer id;
     private String name;
     private String description;
     private double availableQuantity;
     private BigDecimal price;
-    private Integer categoryId;
-    private String categoryName;
-    private String categoryDescription;
+    private CategoryResponse category;
 }
